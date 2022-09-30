5f18c3aa9b3b52a42fd99bcd0af4c6afe531bc67
suuuuetsugu <suuuuetsugu@gmail.com>
2022-09-30 17:53:28 +0900

    Create contractsAPI and shippInfoAPI

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingModule } from './logging/logging.module';
 import { UsersModule } from './users/users.module';
+import { ContractsModule } from './contracts/contracts.module';
+import { shippInfoModule } from './shippInfo/shippInfo.module';
+import { SubscribeModule } from './subscribe/subscribe.module';
+import { ConfigModule } from '@nestjs/config';
+import { StripeModule } from 'nestjs-stripe';
 
 @Module({
-  imports: [LoggingModule, UsersModule],
+  imports: [LoggingModule, UsersModule, ContractsModule, shippInfoModule, SubscribeModule, ConfigModule, StripeModule],
   controllers: [AppController],
   providers: [AppService],
 })
