3440e91b812097dcb709c7779609a36b344fd958
yuko1113 <y.nemo25@gmail.com>
2022-10-07 13:50:39 +0900

    パーソナライズのCSSとログイン・非ログインの表示出し分け

--- frontend/next/pages/recommend.tsx
+++ frontend/next/pages/recommend.tsx
@@ -1,10 +1,31 @@
-import { useRouter } from "next/router";
-import Image from "next/image";
-import Link from "next/link";
-import axios, { AxiosResponse, AxiosError } from "axios";
-import React, { useState, useEffect, ChangeEvent } from "react";
-import Layout from "../layout/Layout";
-import { hasCookie } from "cookies-next";
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Link from 'next/link';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import Layout from '../layout/Layout';
+import { hasCookie } from 'cookies-next';
+import {
+  Button,
+  TextField,
+  Fab,
+  Box,
+  Container,
+  Stack,
+  Paper,
+  Grid,
+} from '@mui/material';
+import { styled } from '@mui/system';
+import {
+  ShoppingCart,
+  KeyboardArrowRight,
+  KeyboardArrowLeft,
+} from '@mui/icons-material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 type category = {
   id: number;
@@ -19,19 +40,63 @@ type product = {
   imgUrl: string;
 };
 
+const StyledBox = styled(Box)(({ theme }) => ({
+  opacity: 1,
+  textAlign: 'center',
+  color: '#333333',
+}));
+
+const SelectBtn = styled(Button, {
+  name: 'SelectBtn',
+})({
+  width: 176,
+  height: 48,
+  margin: 8,
+  borderRadius: 2,
+  fontWeight: '600',
+  '&:hover': {
+    background: '#FFF262',
+  },
+  variant: 'contained',
+  color: 'secondary',
+});
+
+const Label = styled('label')`
+  padding: 0 0 4px;
+  line-height: 1.5;
+  display: block;
+  fontsize: 14;
+`;
+
+const Item = styled(Box)(({ theme }) => ({
+  backgroundColor: '#F2F2F2',
+  padding: theme.spacing(1),
+  textAlign: 'center',
+}));
+
 export default function recommend() {
   const router = useRouter();
   const [category, setCategory] = useState<category>();
   const [products, setProducts] = useState<product[]>();
   const [auth, setAuth] = useState(false);
+  const [selectPlan, setSelectPlan] = useState('0');
+  const [selectCycle, setSelectCycle] = useState('4');
+
+  const planChange = (event: SelectChangeEvent) => {
+    setSelectPlan(event.target.value);
+  };
+
+  const cycleChange = (event: SelectChangeEvent) => {
+    setSelectCycle(event.target.value);
+  };
 
   const categoryNum = Number(router.query.category) - 1;
 
   const comment = [
-    "バランスの取れた食事が必要です！",
-    "タンパク質やビタミンを取りましょう！",
-    "カルシウムを取りましょう！",
-    "鉄やビタミンCを取りましょう！",
+    'バランスの取れた食事が必要です！',
+    'タンパク質やビタミンを取りましょう！',
+    'カルシウムを取りましょう！',
+    '鉄やビタミンCを取りましょう！',
   ];
 
   useEffect(() => {
@@ -53,73 +118,194 @@ export default function recommend() {
         setProducts(res.data);
       })
       .catch((e: AxiosError<{ error: string }>) => console.log(e));
+
+    const signedIn = hasCookie('signedIn');
+    const loginuser = () => {
+      if (signedIn == true) {
+        setAuth(true);
+      }
+    };
+    loginuser();
   }, [router.isReady, router.query.category]);
 
   console.log(category);
   console.log(products);
-
-  const signedIn = hasCookie("signedIn");
-  const loginuser = () => {
-    if (signedIn == true) {
-      setAuth(true);
-    }
-  };
-  loginuser();
+  console.log(auth);
 
   return (
     <>
       <Layout auth={auth}>
-        <div>
-          {router.query.name}さんの診断結果
-          <br />
-          {comment[categoryNum]}
-          <br />
-          <br />
-          {category ? <h2>おすすめプラン {category.name} plan</h2> : ""}
-          <br />
+        <Container maxWidth='lg'>
+          <Stack spacing={2}>
+            <StyledBox
+              sx={{
+                fontSize: '24px',
+                fontWeight: '700',
+                marginTop: '24px',
+              }}
+            >
+              {router.query.name}さんの診断結果
+            </StyledBox>
+            <StyledBox
+              sx={{
+                fontSize: '15px',
+                fontWeight: '600',
+              }}
+            >
+              {comment[categoryNum]}
+            </StyledBox>
+          </Stack>
           {products ? (
             <>
-              <div>
-                <Image src={products[0].imgUrl} width={320} height={320} />
-              </div>
-              <br />
+              <StyledBox>
+                <Image
+                  src={products[0].imgUrl}
+                  width={320}
+                  height={320}
+                  style={{ alignSelf: 'center' }}
+                />
+              </StyledBox>
+              <Stack spacing={1}>
+                <StyledBox
+                  sx={{
+                    fontSize: '16px',
+                    fontWeight: '700',
+                  }}
+                >
+                  おすすめプラン
+                </StyledBox>
+                <StyledBox
+                  sx={{
+                    fontSize: '16px',
+                    fontWeight: '700',
+                  }}
+                >
+                  {category ? category.name : ''} plan
+                </StyledBox>
+                <StyledBox
+                  sx={{
+                    fontSize: '14px',
+                    fontWeight: '500',
+                  }}
+                >
+                  {products ? products[0].introduction : ''}
+                </StyledBox>
+              </Stack>
             </>
           ) : (
-            <div></div>
+            <Box></Box>
           )}
-          {products ? products[0].introduction : ""}
-          <br />
           {products ? (
             <>
-              <label>
-                {products[0].name} {products[0].price}円（税込）
-              </label>
-              <Link
-                href={{ pathname: "/signup", query: { id: products[0].id } }}
-                as="signup"
-              >
-                <button>購入手続きへ</button>
-              </Link>{" "}
-              <br />
-              <label>
-                {products[1].name} {products[1].price}円（税込）
-              </label>
-              <Link
-                href={{ pathname: "/signup", query: { id: products[1].id } }}
-                as="/signup"
-              >
-                <button>購入手続きへ</button>
-              </Link>
+              {!auth ? ( //新規の人だったら、プラン選択と配送サイクル選択をしてもらう
+                <>
+                  <Stack
+                    direction={{ xs: 'column', sm: 'row' }}
+                    spacing={{ xs: 1, sm: 2, md: 4 }}
+                    sx={{
+                      my: 4,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <FormControl sx={{ m: 1, minWidth: 288 }}>
+                      <FormHelperText>契約プラン</FormHelperText>
+                      <Select
+                        value={selectPlan}
+                        onChange={planChange}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                      >
+                        <MenuItem value={0}>
+                          {products[0].name}{' '}
+                          {Number(products[0].price).toLocaleString()}
+                          円（税込）
+                        </MenuItem>
+                        <MenuItem value={1}>
+                          {products[1].name}{' '}
+                          {Number(products[1].price).toLocaleString()}
+                          円（税込）
+                        </MenuItem>
+                      </Select>
+                    </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 288 }}>
+                      <FormHelperText>配送サイクル</FormHelperText>
+                      <Select
+                        value={selectCycle}
+                        onChange={cycleChange}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                      >
+                        <MenuItem value={4}>2週間ごと</MenuItem>
+                        <MenuItem value={2}>3週間ごと</MenuItem>
+                        <MenuItem value={0}>4週間ごと</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Stack>
+                  <StyledBox>
+                    <Link
+                      href={{
+                        pathname: '/signup',
+                        query: {
+                          id: products[Number(selectPlan) + Number(selectCycle)]
+                            .id,
+                        },
+                      }}
+                      as='signup'
+                    >
+                      <SelectBtn
+                        variant='contained'
+                        color='secondary'
+                        startIcon={<ShoppingCart />}
+                        endIcon={<KeyboardArrowRight />}
+                      >
+                        購入手続きへ
+                      </SelectBtn>
+                    </Link>
+                  </StyledBox>
+                </>
+              ) : (
+                // ログインユーザーだったら、マイページに遷移してから契約変更してもらう
+                <StyledBox>
+                  <Link
+                    href={{
+                      pathname: '/mypage',
+                      query: {
+                        id: products[Number(selectPlan) + Number(selectCycle)]
+                          .id,
+                      },
+                    }}
+                    as='mypage'
+                  >
+                    <SelectBtn
+                      variant='contained'
+                      color='secondary'
+                      startIcon={<ShoppingCart />}
+                      endIcon={<KeyboardArrowRight />}
+                    >
+                      契約変更手続きへ
+                    </SelectBtn>
+                  </Link>
+                </StyledBox>
+              )}
             </>
           ) : (
-            <div></div>
+            <></>
           )}
-          <br />
-          <br />
-          <Link href="/questions">
-            <button>もう1度診断する</button>
-          </Link>
-        </div>
+          <StyledBox>
+            <Link href='/questions'>
+              <SelectBtn
+                variant='contained'
+                color='secondary'
+                startIcon={<KeyboardArrowLeft />}
+              >
+                もう1度診断する
+              </SelectBtn>
+            </Link>
+          </StyledBox>
+        </Container>
       </Layout>
     </>
   );
