ab95e61970f2b8554563e94a736928e35bf71935
yuko1113 <y.nemo25@gmail.com>
2022-10-11 23:30:09 +0900

    signup,loginのCSS完了

--- frontend/next/pages/login.tsx
+++ frontend/next/pages/login.tsx
@@ -2,14 +2,50 @@ import React, { SyntheticEvent, useState } from 'react';
 // import axios, { AxiosResponse, AxiosError } from "axios";
 import Layout from '../layout/Layout';
 import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import Link from 'next/link';
 import { setCookie } from 'cookies-next';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import styled from '@mui/system/styled';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+
+interface Inputs {
+  mail: string;
+  password: string;
+  pass: string;
+}
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: '#333333',
+}));
+
+const StyledOutlinedInput = styled(OutlinedInput, {
+  name: 'StyledOutlinedInput',
+})({
+  backgroundColor: 'white',
+  width: 288,
+  height: 40,
+});
 
 export default function Login() {
+  const {
+    register,
+    formState: { errors },
+  } = useForm<Inputs>();
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const Submit = async (e: SyntheticEvent) => {
     e.preventDefault();
@@ -34,38 +70,122 @@ export default function Login() {
       });
   };
 
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <Layout>
-        <h3>ログイン</h3>
-        <form>
-          <div>
-            <label>メールアドレス</label>
-            <input
-              type='text'
-              placeholder='mail@example.com'
-              onInput={(e) => setEmail(e.currentTarget.value)}
-              required
-            />
-          </div>
-          <div>
-            <label>パスワード</label>
-            <input
-              type='password'
-              placeholder='半角英数字で８文字以上'
-              onInput={(e) => setPassword(e.currentTarget.value)}
-              required
-            />
-          </div>
-
-          <button type='submit' onClick={Submit}>
+        <Box
+          sx={{
+            my: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '24px',
+          }}
+        >
+          <StyledBox
+            sx={{
+              fontSize: '24px',
+              fontWeight: '700',
+            }}
+          >
             ログイン
-          </button>
-        </form>
-        <div>
-          femeatsのアカウントを作成する場合は
+          </StyledBox>
+          <FormControl sx={{ m: 1, minWidth: 288 }}>
+            <FormHelperText
+              sx={{
+                fontSize: '14px',
+                fontWeight: '700',
+                marginTop: '24px',
+                marginBottom: '8px',
+              }}
+            >
+              メールアドレス
+            </FormHelperText>
+            <StyledOutlinedInput
+              type='text'
+              {...register('mail', { required: true, maxLength: 30 })}
+              placeholder='mail@example.com'
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+          </FormControl>
+          <FormControl sx={{ m: 1, minWidth: 288 }}>
+            <FormHelperText
+              sx={{
+                fontSize: '14px',
+                fontWeight: '700',
+                marginTop: '16px',
+              }}
+            >
+              パスワード
+            </FormHelperText>
+            <FormHelperText
+              sx={{
+                fontSize: '12px',
+                fontWeight: '400',
+              }}
+            >
+              大文字含む8文字以上の半角英数記号
+            </FormHelperText>
+            <StyledOutlinedInput
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge='end'
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <Button
+            sx={{
+              borderRadius: 16,
+              fontSize: '0.875rem',
+              fontWeight: '700',
+              backgroundColor: '#333333',
+              width: 242,
+              height: 48,
+              marginTop: 4,
+              '&:hover': {
+                color: 'primary.main',
+                background: '#FFF262',
+              },
+            }}
+            variant='contained'
+            color='primary'
+            type='submit'
+            onClick={Submit}
+          >
+            ログイン
+          </Button>
+        </Box>
+        <StyledBox
+          sx={{
+            fontSize: '12px',
+            fontWeight: '400',
+            color: '#182222',
+          }}
+        >
+          アカウントを作成する場合は
           <Link href='questions'>パーソナライズ診断</Link>から
-        </div>
+        </StyledBox>
       </Layout>
     </>
   );
