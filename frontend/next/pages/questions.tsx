a54c25c3def5cf84d16bcf18c33d62be45fded04
yuko1113 <y.nemo25@gmail.com>
2022-10-12 20:08:48 +0900

    画像追加、select背景色修正

--- frontend/next/pages/questions.tsx
+++ frontend/next/pages/questions.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
@@ -127,7 +128,7 @@ export default function App(props: questions) {
         },
         'recommend'
       );
-    }, 3 * 1000);
+    }, 5 * 1000);
   };
 
   return (
@@ -189,15 +190,106 @@ export default function App(props: questions) {
                 </Button>
               </Box>
             ) : waitingResult ? (
-              <Box
-                sx={{
-                  fontSize: '16px',
-                  fontWeight: '700',
-                  marginTop: '16px',
-                }}
-              >
-                {userName}さんにおすすめのプランを診断中
-              </Box>
+              <Container maxWidth='lg'>
+                <Box
+                  sx={{
+                    my: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: 8,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      mr: 2,
+                      position: 'absolute',
+                      top: '10%',
+                      right: '50%',
+                      // transform: 'translate(-50%, -50%)',
+                      display: { xs: 'block', sm: 'none' },
+                    }}
+                  >
+                    <Image
+                      src='/left.png'
+                      width={200}
+                      height={120}
+                      className={styles.fuwafuwa}
+                    />
+                  </Box>
+                  <Box
+                    sx={{
+                      mr: 2,
+                      position: 'absolute',
+                      top: '20%',
+                      right: '50%',
+                      transform: 'translate(-50%, -50%)',
+                      display: { xs: 'none', sm: 'block' },
+                    }}
+                  >
+                    <Image
+                      src='/left.png'
+                      width={200}
+                      height={120}
+                      className={styles.fuwafuwa}
+                    />
+                  </Box>
+                  <Box
+                    sx={{
+                      fontSize: '16px',
+                      fontWeight: '700',
+                      marginTop: '80px',
+                      display: { xs: 'block', sm: 'none' },
+                    }}
+                  >
+                    {userName}さんにおすすめのプランを診断中
+                  </Box>
+                  <Box
+                    sx={{
+                      fontSize: '16px',
+                      fontWeight: '700',
+                      marginTop: '80px',
+                      display: { xs: 'none', sm: 'block' },
+                    }}
+                  >
+                    {userName}さんにおすすめのプランを診断中
+                  </Box>
+                  <Box
+                    sx={{
+                      mr: 2,
+                      position: 'absolute',
+                      top: '35%',
+                      right: '5%',
+                      display: { xs: 'block', sm: 'none' },
+                    }}
+                  >
+                    <Image
+                      src='/right.png'
+                      width={180}
+                      height={120}
+                      className={styles.fuwafuwa}
+                    />
+                  </Box>
+                  <Box
+                    sx={{
+                      mr: 2,
+                      position: 'absolute',
+                      top: '45%',
+                      left: '65%',
+                      transform: 'translate(-50%, -50%)',
+                      display: { xs: 'none', sm: 'block' },
+                    }}
+                  >
+                    <Image
+                      src='/right.png'
+                      width={180}
+                      height={120}
+                      className={styles.fuwafuwa}
+                    />
+                  </Box>
+                </Box>
+              </Container>
             ) : (
               <>
                 <Box
@@ -211,14 +303,64 @@ export default function App(props: questions) {
                   }}
                 >
                   <Stack spacing={1}>
+                    <Box
+                      sx={{
+                        mr: 2,
+                        position: 'absolute',
+                        top: '17%',
+                        right: '59%',
+                        display: { xs: 'block', sm: 'none' },
+                      }}
+                    >
+                      <Image src='/left.png' width={150} height={90} />
+                    </Box>
                     <StyledBox
                       sx={{
                         fontSize: '24px',
                         fontWeight: '700',
                       }}
                     >
+                      <Box
+                        sx={{
+                          mr: 2,
+                          position: 'absolute',
+                          top: '20%',
+                          right: '53%',
+                          display: { xs: 'none', sm: 'block' },
+                        }}
+                      >
+                        <Image src='/left.png' width={150} height={90} />
+                      </Box>
+                      <StyledBox
+                        sx={{
+                          fontSize: '24px',
+                          fontWeight: '700',
+                        }}
+                      ></StyledBox>
                       Question
                     </StyledBox>
+                    <Box
+                      sx={{
+                        mr: 2,
+                        position: 'absolute',
+                        top: '16%',
+                        right: '2%',
+                        display: { xs: 'block', sm: 'none' },
+                      }}
+                    >
+                      <Image src='/right.png' width={144} height={96} />
+                    </Box>
+                    <Box
+                      sx={{
+                        mr: 2,
+                        position: 'absolute',
+                        top: '20%',
+                        right: '36%',
+                        display: { xs: 'none', sm: 'block' },
+                      }}
+                    >
+                      <Image src='/right.png' width={144} height={96} />
+                    </Box>
                     <StyledBox
                       sx={{
                         fontSize: '24px',
